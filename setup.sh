9176deb753d99c4c0a93d23fe186b8749ae12718
brennantduck@gmail.com <brennantduck@gmail.com>
2026-05-18 15:47:24 -0400

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install ansible git
-git clone <your-playbook-repo>
-cd <your-playbook-repo>
+git clone git@github.com:bduckdev/mac_env.git
+cd mac_env
 ansible-playbook local.yml --ask-become-pass
