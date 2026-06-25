6951da127a3753d54f5d38822d501d03ff20ab4e
Brennan Duck <brennantduck@gmail.com>
2026-06-25 11:43:20 -0400

    updated copilot and tabnine

--- dotfiles/nvim/.config/nvim/lua/plugins/copilot.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,9 +1,11 @@
 return {
 	{
-		--	"github/copilot.vim",
+		"github/copilot.vim",
+		enabled = false,
 	},
 	{
 		"codota/tabnine-nvim",
+		enabled = false,
 		build = "./dl_binaries.sh",
 		config = function()
 			require("tabnine").setup({
